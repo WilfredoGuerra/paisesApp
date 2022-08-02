1d047d8d79dec6ed66cb9374a0e39f7fb0c0671c
Wilfredo Guerra <willigue@gmail.com>
2022-08-02 17:05:29 -0500

    commit final de la app

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,6 +7,12 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    
+    `
   ]
 })
 export class PorPaisComponent {
@@ -14,6 +20,8 @@ export class PorPaisComponent {
   termino : string = '';
   hayError: boolean = false;
   paises  : Country[] = [];
+  paisesSugeridos  : Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   buscar(termino: string) {
     this.hayError = false;
@@ -35,7 +43,17 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+    .subscribe( paises => this.paisesSugeridos = paises.splice(0,5),
+    (err) => this.paisesSugeridos = []);
     
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar( termino );
   }
 
 
