9c27739d2164f869a00b294dfce0f2a467b210a2
Wilfredo Guerra <willigue@gmail.com>
2022-08-01 19:54:38 -0500

    primer commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+    .subscribe( paises => {
+      console.log(paises);
+      this.paises = paises;
+    }, (err) => {
+      this.hayError = true;
+      this.paises   = [];
+      
+    })
+  }
+
+  
+
+  constructor( private paisService: PaisService) { }
+
+  sugerencias( termino: string ) {
+    this.hayError = false;
+    
+  }
+
+
+}
